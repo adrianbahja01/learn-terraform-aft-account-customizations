8588ee9e1ddc176c1d693200c5d0f69cfcb9e6f3
Adrian Bahja <adrian.bahja@alpiq.com>
2023-11-13 11:35:55 +0100

    testacct

--- sandbox2/api_helpers/post-api-helpers.sh
+++ sandbox2/api_helpers/post-api-helpers.sh
@@ -1,3 +1,10 @@
 echo "Executing Post-API Helpers"
 echo "Account ID ..."
 aws sts get-caller-identity --query Account --output text
+
+export REGIONS=$(aws ec2 describe-regions | jq -r ".Regions[].RegionName")
+for region in $REGIONS; do
+# list vpcs
+echo $region
+aws --region=$region ec2 describe-vpcs | jq ".Vpcs[]|{is_default: .IsDefault, cidr: .CidrBlock, id: .VpcId} | select(.is_default)"
+done
