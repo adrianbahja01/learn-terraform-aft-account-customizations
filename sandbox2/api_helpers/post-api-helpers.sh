e2d71333a5c003b851e6e6d94df172c427185ef6
Adrian Bahja <adrian.bahja@alpiq.com>
2023-11-13 11:42:54 +0100

    testacct

--- sandbox2/api_helpers/post-api-helpers.sh
+++ sandbox2/api_helpers/post-api-helpers.sh
@@ -8,3 +8,27 @@ for region in $REGIONS; do
 echo $region
 aws --region=$region ec2 describe-vpcs | jq ".Vpcs[]|{is_default: .IsDefault, cidr: .CidrBlock, id: .VpcId} | select(.is_default)"
 done
+
+for region in $REGIONS ; do
+echo "Destroy in $region"
+# list vpcs
+export IDs=$(aws --region=$region ec2 describe-vpcs | jq -r ".Vpcs[]|{is_default: .IsDefault, id: .VpcId} | select(.is_default) | .id")
+for id in "$IDs" ; do
+if [ -z "$id" ] ; then
+ continue
+fi
+# destroy igws
+for igw in `aws --region=$region ec2 describe-internet-gateways | jq -r ".InternetGateways[] | {id: .InternetGatewayId, vpc: .Attachments[0].VpcId} | select(.vpc == \"$id\") | .id"` ; do
+echo "Destroy igw $region $id $igw"
+aws --region=$region ec2 detach-internet-gateway --internet-gateway-id=$igw --vpc-id=$id
+aws --region=$region ec2 delete-internet-gateway --internet-gateway-id=$igw
+done
+# destroy subnets
+for sub in `aws --region=$region ec2 describe-subnets | jq -r ".Subnets[] | {id: .SubnetId, vpc: .VpcId} | select(.vpc == \"$id\") | .id"` ; do
+echo "Destroy subnet $region $id $sub"
+aws --region=$region ec2 delete-subnet --subnet-id=$sub
+done
+echo "Destroy vpc $region $id"
+aws --region=$region ec2 delete-vpc --vpc-id=$id
+done
+done
